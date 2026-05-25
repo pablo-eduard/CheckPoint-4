c99cde13ea033ba789afde4b1d4fe8adee3130f0
pablo-eduard <pablitogoncalves2008@gmail.com>
2026-05-24 23:44:02 -0300

    Comit do código completoi

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-# main.py
 from tela import janela
 from modelo import criar_tabela
 
 if __name__ == "__main__":
-    # 1. Garante que a tabela exista antes de iniciar a interface
+    
     criar_tabela()
     
-    # 2. Inicia o loop principal da janela do CustomTkinter
     janela.mainloop()
